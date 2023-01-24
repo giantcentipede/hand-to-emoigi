d31802035ed3013e8b7ed6545b36436e6cf771af
giantcentipede <97794708+giantcentipede@users.noreply.github.com>
2023-01-24 16:51:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+prediction_1 = "";
+prediction_2= "";
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera"); 
+
+function take_snapshot() {
+Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML='<img id="capture_image" src="'+data_uri+'"/>';
+
+});
+}
+
+console.log("ml5 version=",ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/hHuFwuljR/model.json',modelLoaded);
+function modelLoaded() {
+    console.log("modelloaded");
+
+}
+
+function check() {
+    img= document.getElementById("capture_image");
+    classifier.classify(img,gotResults);
+}
+
+function speak() {
+    var synth=window.speechSynthesis;
+    speak_data_1 = "the first prediction is" + prediction_1;
+    speak_data_2 = "and the second prediction is" + prediction_2;
+    var utterThis=new SpeechSynthesisUtterance(speak_data_1+speak_data_2);
+    synth.speak(utterThis);
+    
+}
+
+function gotResults(error,results) {
+if (error) {
+    console.error(error);
+}
+else {
+    console.log(results);
+    document.getElementById("result_emotion_name").innerHTML=results[0].label;
+    document.getElementById("result_emotion_name2").innerHTML=results[1].label;
+    prediction_1 = results[0].label;
+    prediction_2 = results[1].label;
+    speak();
+
+    if (results[0].label=="fist") {
+document.getElementById("update_emoji").innerHTML="&#9994;";
+ }
+ if (results[0].label=="5") {
+ document.getElementById("update_emoji").innerHTML="&#9995;";
+ }
+ if (results[0].label=="ok") {
+document.getElementById("update_emoji").innerHTML="&#128076;";
+}
+if (results[0].label=="victory") {
+    document.getElementById("update_emoji").innerHTML="&#9996;";
+     }
+}}
